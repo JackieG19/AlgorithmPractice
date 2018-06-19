56fa28edec2604639fd00bc4314515e1a7436efc
Jackie <JGtheophila19@gmail.com>
2018-06-19 13:07:27 -0400

    Create board2.js

---
+++ board2.js
@@ -0,0 +1,16 @@
+// single stack
+
+/*
+
+Q        | New stacks |                     temp 5 ---- R1   1
+         |            |                     temp 4 ---- R2   1
+5        5     5      5     5!              temp 3 ---- R3   1
+4        4     4      4                     temp 2 ---- R4   1
+3        3     3                                             ^
+2        2                                   return 1--------|
+1         --->1
+
+5x4
+5x4x3
+
+*/
