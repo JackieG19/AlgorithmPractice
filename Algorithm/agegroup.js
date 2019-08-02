9d004429c0987f2b9f1cbb7aaef03263a1614a19
Jackie <JGtheophila19@gmail.com>
2019-08-02 18:13:07 -0400

    finding persons with the same input age

---
+++ Algorithm/agegroup.js
@@ -0,0 +1,22 @@
+var object = [
+    {name:'Cliff',
+    age: 32},
+
+    {name: 'Taka',
+    age: 25},
+
+    {name: 'Andy',
+    age: 32},
+
+    {name: 'Roy',
+    age: 21},
+
+    {name: 'Sally',
+    age: 25}
+    ];
+
+for(var i = 0; i < object.length; i++){
+   if(object[i].age == 25){
+       console.log(object[i]);
+   }
+}
